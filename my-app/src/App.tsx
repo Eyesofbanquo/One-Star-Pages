d3ae458548d79893d97b9ad20e8ad8a97bbcf299
Markim Shaw <ms79723@gmail.com>
2023-08-01 13:27:38 -0500

    add items

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import mainLogo from "./friend_score_logo.png";
+import "./App.css";
 
-function App() {
+const App = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={mainLogo} style={{ width: 200, height: 200 }} alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          <code style={{ color: "black" }}>
+            <b>Friend Score</b>
+          </code>
+        </p>
+        <p>
+          <i style={{ color: "black" }}>Everyone has a score</i>
         </p>
         <a
+          style={{ color: "black" }}
           className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          href="mailto:ms79723@gmail.com"
         >
-          Learn React
+          Contact for support
         </a>
       </header>
     </div>
   );
-}
+};
 
 export default App;
